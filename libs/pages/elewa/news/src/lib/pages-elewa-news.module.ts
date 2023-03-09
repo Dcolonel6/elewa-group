dcd1ff2239b1021bcc7d47d19aa6c90f46862983
Dennis Njogu <Dcolonel6@users.noreply.github.com>
2023-03-09 15:35:07 +0300

    :feat: filter articles

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { NewsSectionComponent } from './components/news-section/news-section.component';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
+import { ButtonPlainComponent } from './components/button-plain/button-plain.component';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
+  declarations: [NewsPageComponent, NewsSectionComponent, ButtonPlainComponent],
 })
 export class NewsPageModule {}
